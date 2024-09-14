e0ef58afc6f9f0fc4a309d9d3ee8909a52695b12
Simmi Keshri <b7wuuw2o@students.codeinstitute.net>
2024-09-14 21:12:54 +0000

    added post_detail view

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['8000-simmik-djangoblog-q50j6xr8isp.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-simmik-djangoblog-q50j6xr8isp.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
